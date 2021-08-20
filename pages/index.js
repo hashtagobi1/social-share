783bf75b2f3dda212f60536eb9d652075f8ec038
obi <anokwuruobi@gmail.com>
2021-08-20 22:24:34 +0100

    ✨ added an Empty State

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
-import { Heading, Page } from "@shopify/polaris";
+import { EmptyState, Heading, Layout, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+    <Layout>
+      <EmptyState
+        heading="create discounts for customers"
+        action={{
+          content: 'Create Discounts',
+          onAction: () => this.setState({ open: true }),
+        }}
+        image="http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/potato.png"
+      >
+
+      </EmptyState>
+    </Layout>
+    <Heading>Obi App!</Heading>
   </Page>
 );
 
