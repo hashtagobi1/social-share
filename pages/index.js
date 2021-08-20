6895293d10267af0b69517dcf10d5748279e02e2
obi <anokwuruobi@gmail.com>
2021-08-20 22:40:59 +0100

    🎨  created state for button being opened

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import { EmptyState, Heading, Layout, Page } from "@shopify/polaris";
+import React, { useState } from 'react'
 
-const Index = () => (
-  <Page>
-    <Layout>
-      <EmptyState
-        heading="create discounts for customers"
-        action={{
-          content: 'Create Discounts',
-          onAction: () => this.setState({ open: true }),
-        }}
-        image="http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/potato.png"
-      >
 
-      </EmptyState>
-    </Layout>
-    <Heading>Obi App!</Heading>
-  </Page>
-);
+const Index = () => {
+
+  const [open, setOpen] = useState(false)
+
+  if (open) {
+    console.log('Clicked!!')
+  }
+
+
+  return (
+    <Page>
+      <Layout>
+        <EmptyState
+          heading="Discount your products temporarily"
+          action={{
+            content: 'Select Products',
+            onAction: () => setOpen(true)
+          }}
+          image="http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/potato.png"
+        >
+          <p>Select products to change their price temporarily.</p>
+
+        </EmptyState>
+      </Layout>
+      <Heading>Obi App!</Heading>
+    </Page>
+
+  )
+};
 
 export default Index;
